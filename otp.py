cd90d43d7a8f7a87310f192a76646e0cb7357e6e
A1-sobharani <83705126+A1-sobharani@users.noreply.github.com>
2021-05-28 17:49:24 +0530

    Add files via upload

---
+++ otp.py
@@ -0,0 +1,9 @@
+from random import randint
+
+for i in range(6):
+    print(randint(0,9),
+          randint(0,9),
+          randint(0,9),
+          randint(0,9),
+          randint(0,9),
+          randint(0,9), sep = '')
